f896f42f7250b4535169b70c93059e64f08fd17a
asliddin <asliddintojaliyev88@gmail.com>
2022-11-12 13:37:35 +0500

    commit

---
+++ src/main/java/com/example/trelloapiwithspringboot/dtos/card/CardChangeColumnDTO.java
@@ -0,0 +1,9 @@
+package com.example.trelloapiwithspringboot.dtos.card;
+
+/**
+ * @author "Tojaliyev Asliddin"
+ * @since 12/11/22 13:34 (Saturday)
+ * trello-api-with-Spring-Boot/IntelliJ IDEA
+ */
+public class CardChangeColumnDTO {
+}
