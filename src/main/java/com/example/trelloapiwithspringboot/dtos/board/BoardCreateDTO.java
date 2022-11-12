32e54ab1fe88394800e36bda1e04f02d608f19b6
asliddin <asliddintojaliyev88@gmail.com>
2022-11-12 13:27:09 +0500

    commit

---
+++ src/main/java/com/example/trelloapiwithspringboot/dtos/board/BoardCreateDTO.java
@@ -0,0 +1,9 @@
+package com.example.trelloapiwithspringboot.dtos.board;
+
+/**
+ * @author "Tojaliyev Asliddin"
+ * @since 12/11/22 13:25 (Saturday)
+ * trello-api-with-Spring-Boot/IntelliJ IDEA
+ */
+public class BoardCreateDTO {
+}
