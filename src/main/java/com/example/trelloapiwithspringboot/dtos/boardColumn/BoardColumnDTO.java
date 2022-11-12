e658a64ec220855e65b2fddcd2f6ef8482107b20
asliddin <asliddintojaliyev88@gmail.com>
2022-11-12 13:33:09 +0500

    commit

---
+++ src/main/java/com/example/trelloapiwithspringboot/dtos/boardColumn/BoardColumnDTO.java
@@ -0,0 +1,9 @@
+package com.example.trelloapiwithspringboot.dtos.boardColumn;
+
+/**
+ * @author "Tojaliyev Asliddin"
+ * @since 12/11/22 13:31 (Saturday)
+ * trello-api-with-Spring-Boot/IntelliJ IDEA
+ */
+public class BoardColumnDTO {
+}
---
+++ src/main/java/com/example/trelloapiwithspringboot/dtos/boardColumn/BoardColumnOrderChangeDTO.java
@@ -0,0 +1,9 @@
+package com.example.trelloapiwithspringboot.dtos.boardColumn;
+
+/**
+ * @author "Tojaliyev Asliddin"
+ * @since 12/11/22 13:32 (Saturday)
+ * trello-api-with-Spring-Boot/IntelliJ IDEA
+ */
+public class BoardColumnOrderChangeDTO {
+}
