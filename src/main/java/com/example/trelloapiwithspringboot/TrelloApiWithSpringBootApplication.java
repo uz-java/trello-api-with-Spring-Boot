bda2f3499443592cb02ef452ca2ccc9b7340aff0
asliddin <asliddintojaliyev88@gmail.com>
2022-11-12 10:56:41 +0500

    commit

---
+++ src/main/java/com/example/trelloapiwithspringboot/TrelloApiWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.trelloapiwithspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloApiWithSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloApiWithSpringBootApplication.class, args);
+    }
+
+}
