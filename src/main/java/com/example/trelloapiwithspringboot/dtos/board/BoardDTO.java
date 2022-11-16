7639b5f5d5daec3eb84850caf378598c804bb952
asliddin <asliddintojaliyev88@gmail.com>
2022-11-16 17:11:48 +0500

    commit

--- src/main/java/com/example/trelloapiwithspringboot/dtos/board/BoardDTO.java
+++ src/main/java/com/example/trelloapiwithspringboot/dtos/board/BoardDTO.java
@@ -1,9 +1,25 @@
 package com.example.trelloapiwithspringboot.dtos.board;
 
+import com.example.trelloapiwithspringboot.dtos.boardColumn.BoardColumnDTO;
+import com.example.trelloapiwithspringboot.enums.board.BoardVisibilityType;
+import lombok.*;
+
+import java.util.Set;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 12/11/22 13:25 (Saturday)
  * trello-api-with-Spring-Boot/IntelliJ IDEA
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class BoardDTO {
+    private Long id;
+    private String name;
+    private BoardVisibilityType visibilityType;
+    private Long workspaceId;
+    private Set<BoardColumnDTO> boardColumnDTOS;
 }
--- src/main/java/com/example/trelloapiwithspringboot/dtos/board/BoardUpdateDTO.java
+++ src/main/java/com/example/trelloapiwithspringboot/dtos/board/BoardUpdateDTO.java
@@ -1,11 +1,18 @@
 package com.example.trelloapiwithspringboot.dtos.board;
 
 import com.example.trelloapiwithspringboot.dtos.base.GenericDTO;
+import lombok.*;
 
 /**
  * @author "Tojaliyev Asliddin"
  * @since 12/11/22 13:26 (Saturday)
  * trello-api-with-Spring-Boot/IntelliJ IDEA
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
 public class BoardUpdateDTO extends GenericDTO {
+
 }
