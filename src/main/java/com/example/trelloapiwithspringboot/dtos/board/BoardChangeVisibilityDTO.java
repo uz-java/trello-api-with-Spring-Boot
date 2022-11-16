c203b2850e37f5524a29103c9fd0fe3e50abd05b
asliddin <asliddintojaliyev88@gmail.com>
2022-11-16 17:04:22 +0500

    commit

--- src/main/java/com/example/trelloapiwithspringboot/dtos/board/BoardChangeVisibilityDTO.java
+++ src/main/java/com/example/trelloapiwithspringboot/dtos/board/BoardChangeVisibilityDTO.java
@@ -1,9 +1,19 @@
 package com.example.trelloapiwithspringboot.dtos.board;
 
+import com.example.trelloapiwithspringboot.enums.board.BoardVisibilityType;
+import lombok.*;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 12/11/22 13:25 (Saturday)
  * trello-api-with-Spring-Boot/IntelliJ IDEA
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
 public class BoardChangeVisibilityDTO {
+    private Long id;
+    private BoardVisibilityType visibilityType;
 }
--- src/main/java/com/example/trelloapiwithspringboot/dtos/board/BoardCreateDTO.java
+++ src/main/java/com/example/trelloapiwithspringboot/dtos/board/BoardCreateDTO.java
@@ -1,11 +1,22 @@
 package com.example.trelloapiwithspringboot.dtos.board;
 
 import com.example.trelloapiwithspringboot.dtos.base.BaseGenericDTO;
+import com.example.trelloapiwithspringboot.enums.board.BoardVisibilityType;
+import lombok.*;
 
 /**
  * @author "Tojaliyev Asliddin"
  * @since 12/11/22 13:25 (Saturday)
  * trello-api-with-Spring-Boot/IntelliJ IDEA
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
 public class BoardCreateDTO implements BaseGenericDTO {
+    private String name;
+    @Builder.Default
+    private BoardVisibilityType visibilityType=BoardVisibilityType.PUBLIC;
+    private Long workspaceId;
 }
