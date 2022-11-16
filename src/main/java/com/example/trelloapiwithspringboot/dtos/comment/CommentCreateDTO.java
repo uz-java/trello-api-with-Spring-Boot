5f9d579141b49e6636ea281d6e5b230424f58a65
asliddin <asliddintojaliyev88@gmail.com>
2022-11-17 00:43:46 +0500

    commit

--- src/main/java/com/example/trelloapiwithspringboot/dtos/comment/CommentCreateDTO.java
+++ src/main/java/com/example/trelloapiwithspringboot/dtos/comment/CommentCreateDTO.java
@@ -1,9 +1,20 @@
 package com.example.trelloapiwithspringboot.dtos.comment;
 
+import lombok.*;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 12/11/22 13:38 (Saturday)
  * trello-api-with-Spring-Boot/IntelliJ IDEA
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CommentCreateDTO {
+    private String creatorEmail;
+    private String text;
+    private String attachment;
+    private Long cardId;
 }
--- src/main/java/com/example/trelloapiwithspringboot/dtos/comment/CommentDTO.java
+++ src/main/java/com/example/trelloapiwithspringboot/dtos/comment/CommentDTO.java
@@ -1,9 +1,21 @@
 package com.example.trelloapiwithspringboot.dtos.comment;
 
+import lombok.*;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 12/11/22 13:38 (Saturday)
  * trello-api-with-Spring-Boot/IntelliJ IDEA
  */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CommentDTO {
+    private Long id;
+    private String creator;
+    private String text;
+    private String attachment;
+    private Long cardId;
 }
