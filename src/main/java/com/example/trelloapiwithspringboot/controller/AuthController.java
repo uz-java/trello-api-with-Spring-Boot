0fc6ea4f26a568b9a3009d8fda9e50c64a957a4b
asliddin <asliddintojaliyev88@gmail.com>
2022-11-26 00:44:40 +0500

    commit

---
+++ src/main/java/com/example/trelloapiwithspringboot/controller/AuthController.java
@@ -0,0 +1,9 @@
+package com.example.trelloapiwithspringboot.controller;
+
+/**
+ * @author "Tojaliyev Asliddin"
+ * @since 26/11/22 00:42 (Saturday)
+ * trello-api-with-Spring-Boot/IntelliJ IDEA
+ */
+public class AuthController {
+}
---
+++ src/main/java/com/example/trelloapiwithspringboot/controller/BoardController.java
@@ -0,0 +1,9 @@
+package com.example.trelloapiwithspringboot.controller;
+
+/**
+ * @author "Tojaliyev Asliddin"
+ * @since 26/11/22 00:43 (Saturday)
+ * trello-api-with-Spring-Boot/IntelliJ IDEA
+ */
+public class BoardController {
+}
