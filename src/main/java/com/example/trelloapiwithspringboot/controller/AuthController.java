2cd43507f9d3ff01460527beb863257ab8fdacbc
asliddin <asliddintojaliyev88@gmail.com>
2022-11-27 00:29:02 +0500

    commit

--- src/main/java/com/example/trelloapiwithspringboot/controller/AuthController.java
+++ src/main/java/com/example/trelloapiwithspringboot/controller/AuthController.java
@@ -1,9 +1,19 @@
 package com.example.trelloapiwithspringboot.controller;
 
+import com.example.trelloapiwithspringboot.dtos.auth.LoginRequestDTO;
+import com.example.trelloapiwithspringboot.dtos.auth.UserCreateDTO;
+import com.example.trelloapiwithspringboot.dtos.auth.UserDTO;
+import com.example.trelloapiwithspringboot.dtos.jwt.JwtResponseDTO;
+import com.example.trelloapiwithspringboot.dtos.jwt.RefreshTokenRequest;
 import com.example.trelloapiwithspringboot.service.auth.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * @author "Tojaliyev Asliddin"
@@ -15,4 +25,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
+
+    @PostMapping(value = "/login", produces = "application/json")
+    public ResponseEntity<JwtResponseDTO> login(@RequestBody LoginRequestDTO dto) {
+        return ResponseEntity.ok(userService.login(dto));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<UserDTO> register(@Valid @RequestBody UserCreateDTO dto) {
+        return ResponseEntity.ok(userService.register(dto));
+    }
+
+    @PostMapping("/refreshToken")
+    public ResponseEntity<JwtResponseDTO> refreshToken(@RequestBody RefreshTokenRequest request) {
+        return ResponseEntity.ok(userService.refreshToken(request));
+    }
 }
--- src/main/java/com/example/trelloapiwithspringboot/service/auth/UserService.java
+++ src/main/java/com/example/trelloapiwithspringboot/service/auth/UserService.java
@@ -1,9 +1,20 @@
 package com.example.trelloapiwithspringboot.service.auth;
 
+import com.example.trelloapiwithspringboot.dtos.auth.LoginRequestDTO;
+import com.example.trelloapiwithspringboot.dtos.auth.UserCreateDTO;
+import com.example.trelloapiwithspringboot.dtos.auth.UserDTO;
+import com.example.trelloapiwithspringboot.dtos.jwt.JwtResponseDTO;
+import com.example.trelloapiwithspringboot.dtos.jwt.RefreshTokenRequest;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 13/11/22 00:13 (Sunday)
  * trello-api-with-Spring-Boot/IntelliJ IDEA
  */
 public interface UserService {
+    JwtResponseDTO login(LoginRequestDTO dto);
+
+    UserDTO register(UserCreateDTO dto);
+
+    JwtResponseDTO refreshToken(RefreshTokenRequest request);
 }
