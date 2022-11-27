da1bbe062c20f02ed03efdf1e06e1f3917a90ace
asliddin <asliddintojaliyev88@gmail.com>
2022-11-27 22:10:04 +0500

    commit

--- src/main/java/com/example/trelloapiwithspringboot/controller/WorkspaceController.java
+++ src/main/java/com/example/trelloapiwithspringboot/controller/WorkspaceController.java
@@ -1,9 +1,35 @@
 package com.example.trelloapiwithspringboot.controller;
 
+import com.example.trelloapiwithspringboot.dtos.workspace.WorkspaceCreateDTO;
+import com.example.trelloapiwithspringboot.dtos.workspace.WorkspaceDTO;
+import com.example.trelloapiwithspringboot.service.workspace.WorkspaceService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 26/11/22 00:44 (Saturday)
  * trello-api-with-Spring-Boot/IntelliJ IDEA
  */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/workspace")
+@PreAuthorize("isAuthenticated()")
 public class WorkspaceController {
+    private final WorkspaceService workspaceService;
+
+    @PostMapping(value = "/create",produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<WorkspaceDTO> createWorkspaceDTO(@Valid @RequestBody WorkspaceCreateDTO dto){
+        WorkspaceDTO workspaceDTO=workspaceService.save(dto);
+        return new ResponseEntity<>(workspaceDTO, HttpStatus.CREATED);
+    }
 }
--- src/main/java/com/example/trelloapiwithspringboot/service/workspace/WorkspaceService.java
+++ src/main/java/com/example/trelloapiwithspringboot/service/workspace/WorkspaceService.java
@@ -1,9 +1,13 @@
 package com.example.trelloapiwithspringboot.service.workspace;
 
+import com.example.trelloapiwithspringboot.dtos.workspace.WorkspaceCreateDTO;
+import com.example.trelloapiwithspringboot.dtos.workspace.WorkspaceDTO;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 13/11/22 00:19 (Sunday)
  * trello-api-with-Spring-Boot/IntelliJ IDEA
  */
 public interface WorkspaceService {
+    WorkspaceDTO save(WorkspaceCreateDTO dto);
 }
