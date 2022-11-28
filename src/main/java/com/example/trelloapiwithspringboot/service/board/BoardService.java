ff8a68d83dc8f214dc8e2b3c62a58a0c9997f223
asliddin <asliddintojaliyev88@gmail.com>
2022-11-28 21:00:55 +0500

    commit

--- src/main/java/com/example/trelloapiwithspringboot/service/board/BoardService.java
+++ src/main/java/com/example/trelloapiwithspringboot/service/board/BoardService.java
@@ -3,12 +3,14 @@ package com.example.trelloapiwithspringboot.service.board;
 import com.example.trelloapiwithspringboot.dtos.board.BoardChangeVisibilityDTO;
 import com.example.trelloapiwithspringboot.dtos.board.BoardCreateDTO;
 import com.example.trelloapiwithspringboot.dtos.board.BoardDTO;
+import org.springframework.stereotype.Service;
 
 /**
  * @author "Tojaliyev Asliddin"
  * @since 13/11/22 00:14 (Sunday)
  * trello-api-with-Spring-Boot/IntelliJ IDEA
  */
+@Service
 public interface BoardService {
     BoardDTO save(BoardCreateDTO dto);
 
--- src/main/java/com/example/trelloapiwithspringboot/service/board/BoardServiceImpl.java
+++ src/main/java/com/example/trelloapiwithspringboot/service/board/BoardServiceImpl.java
@@ -1,9 +1,38 @@
 package com.example.trelloapiwithspringboot.service.board;
 
+import com.example.trelloapiwithspringboot.dtos.board.BoardChangeVisibilityDTO;
+import com.example.trelloapiwithspringboot.dtos.board.BoardCreateDTO;
+import com.example.trelloapiwithspringboot.dtos.board.BoardDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 13/11/22 00:14 (Sunday)
  * trello-api-with-Spring-Boot/IntelliJ IDEA
  */
-public class BoardServiceImpl {
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class BoardServiceImpl implements BoardService{
+    @Override
+    public BoardDTO save(BoardCreateDTO dto) {
+        return null;
+    }
+
+    @Override
+    public BoardDTO getBoard(Long id) {
+        return null;
+    }
+
+    @Override
+    public Void changeVisibility(BoardChangeVisibilityDTO dto) {
+        return null;
+    }
+
+    @Override
+    public Void deleteBoard(Long id) {
+        return null;
+    }
 }
