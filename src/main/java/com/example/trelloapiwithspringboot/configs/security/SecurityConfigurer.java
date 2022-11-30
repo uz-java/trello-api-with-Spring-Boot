40e6b24f1d508719c925feac766cbcb4271abb47
asliddin <asliddintojaliyev88@gmail.com>
2022-11-30 14:13:49 +0500

    commit

--- src/main/java/com/example/trelloapiwithspringboot/configs/security/SecurityConfigurer.java
+++ src/main/java/com/example/trelloapiwithspringboot/configs/security/SecurityConfigurer.java
@@ -1,9 +1,49 @@
 package com.example.trelloapiwithspringboot.configs.security;
 
+import com.example.trelloapiwithspringboot.configs.security.filters.JwtFilter;
+import com.example.trelloapiwithspringboot.service.auth.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 13/11/22 00:40 (Sunday)
  * trello-api-with-Spring-Boot/IntelliJ IDEA
  */
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
+@RequiredArgsConstructor
 public class SecurityConfigurer {
+    private final UserService userService;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests(expressionInterceptUrlRegistry -> expressionInterceptUrlRegistry
+                .antMatchers("/auth/login",
+                        "/auth/register",
+                        "/swagger-ui/**",
+                        "/api-docs/***")
+                .permitAll()
+                .anyRequest()
+                .authenticated());
+        http.sessionManagement(httpSecuritySessionManagementConfigurer ->
+                httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        http.addFilterBefore(new JwtFilter(userService), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
