a9c880157fa207d2c09df92eefa24d9766f8fe2c
asliddin <asliddintojaliyev88@gmail.com>
2022-11-30 13:38:02 +0500

    commit

--- src/main/java/com/example/trelloapiwithspringboot/configs/encryption/Encoders.java
+++ src/main/java/com/example/trelloapiwithspringboot/configs/encryption/Encoders.java
@@ -1,9 +1,19 @@
 package com.example.trelloapiwithspringboot.configs.encryption;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 13/11/22 00:37 (Sunday)
  * trello-api-with-Spring-Boot/IntelliJ IDEA
  */
+@Configuration
 public class Encoders {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
--- src/main/java/com/example/trelloapiwithspringboot/configs/security/filters/JwtFilter.java
+++ src/main/java/com/example/trelloapiwithspringboot/configs/security/filters/JwtFilter.java
@@ -1,9 +1,48 @@
 package com.example.trelloapiwithspringboot.configs.security.filters;
 
+import com.example.trelloapiwithspringboot.service.auth.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.function.Function;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 13/11/22 00:39 (Sunday)
  * trello-api-with-Spring-Boot/IntelliJ IDEA
  */
-public class JwtFilter {
+@RequiredArgsConstructor
+@Slf4j
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final UserService userService;
+
+    private final static List<String> WHITE_LIST = List.of(
+            "/auth/login",
+            "/auth/register", "/swagger-ui.*",
+            "/api-docs.*", "/auth/refreshToken");
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+    }
+
+    private String parseJwt(HttpServletRequest request){
+        String authorization = request.getHeader("Authorization");
+        String prefix="Bearer ";
+        if (StringUtils.hasText(authorization) && authorization.startsWith(prefix)){
+            return authorization.substring(prefix.length());
+        }
+        return null;
+    }
+
+    private final static Function<String,Boolean> isOpenUrl=(url)->WHITE_LIST.stream().anyMatch(url::matches);
 }
