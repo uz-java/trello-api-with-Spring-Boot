248f8710e551485cd87fb75461b7eadd7ec67ac6
asliddin <asliddintojaliyev88@gmail.com>
2022-11-30 08:36:31 +0500

    commit

--- src/main/java/com/example/trelloapiwithspringboot/configs/openAPI/OpenAPIConfigurer.java
+++ src/main/java/com/example/trelloapiwithspringboot/configs/openAPI/OpenAPIConfigurer.java
@@ -1,9 +1,26 @@
 package com.example.trelloapiwithspringboot.configs.openAPI;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 13/11/22 00:39 (Sunday)
  * trello-api-with-Spring-Boot/IntelliJ IDEA
  */
+@Configuration
+@OpenAPIDefinition(info = @Info(title = "First Step Forward API",version = "v1",
+        description = "This API just for learning purposes",
+        contact = @Contact(name = "Asliddin",url = "https://pdp.uz",email = "asliddintojaliyev88@gmail.com"),
+        license = @License(name = "Apache Foundation",url = "https://apache.org")),security = {@SecurityRequirement(name = "Bearer")})
+@SecurityScheme(name = "Bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",scheme = "Bearer")
 public class OpenAPIConfigurer {
 }
